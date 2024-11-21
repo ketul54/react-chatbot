361bedf8f709468cb200a86be12f2c27c8206479
dvasyliev <dvasyliev.dev@gmail.com>
2024-11-21 20:47:23 +0100

    S03. L03. Adding Text Input and Send Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Chat } from "./components/Chat/Chat";
+import { Controls } from "./components/Controls/Controls";
 import styles from "./App.module.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <div className={styles.ChatContainer}>
         <Chat messages={messages} />
       </div>
+      <Controls />
     </div>
   );
 }
